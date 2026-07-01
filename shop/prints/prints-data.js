aac949e27ee18779a926f0646457585abd18ce6f
Omar-Elsayed08 <141748556+Omar-Elsayed08@users.noreply.github.com>
2026-07-01 02:19:47 -0500

    new

---
+++ shop/prints/prints-data.js
@@ -0,0 +1,41 @@
+/*
+ *   id
+ *   title
+ *   description
+ *   price
+ *   images      array of images, the first is the thumbnail.
+ *   sizes       leave empty for items without sizes
+ *   swatch      "blue" | "orange" | "black"
+ */
+window.SHOP_PRINTS = [
+    {
+        id: "print1",
+        title: "Print 1",
+        description: "test",
+        price: 15,
+        images: [
+            "../../assets/GalleryAssets/visual-art/ArtPiece.png",
+            "../../assets/images/PaperStar.png"
+        ],
+        sizes: [],
+        swatch: "orange"
+    },
+    {
+        id: "print2",
+        title: "Print 2",
+        description: "text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb",
+        price: 15,
+        images: [],
+        sizes: [],
+        swatch: "blue"
+    },
+    {
+        id: "print3",
+        title: "Print 3",
+        description: "text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb",
+        price: 15,
+        images: [],
+        sizes: [],
+        swatch: "black"
+    }
+];
---
+++ shop/shirts/shirts-data.js
@@ -0,0 +1,80 @@
+/*
+ *   id
+ *   title
+ *   description
+ *   price
+ *   images      array of images, the first is the thumbnail.
+ *   sizes
+ *   swatch      "blue" | "orange" | "black"
+ */
+window.SHOP_SHIRTS = [
+    {
+        id: "shirt1",
+        title: "Shirt 1",
+        description: "test",
+        price: 25,
+        images: [
+            "../../assets/images/Shirt.png",
+            "../../assets/images/LazerBaby.png",
+            "../../assets/images/TreeCreature.png",
+            "../../assets/GalleryAssets/visual-art/ArtPiece.png",
+            "../../assets/images/HeadphoneBlob.png"
+        ],
+        sizes: ["S", "M", "L", "XL"],
+        swatch: "blue"
+    },
+    {
+        id: "shirt2",
+        title: "Shirt 2",
+        description: "text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb",
+        price: 25,
+        images: [],
+        sizes: ["S", "M", "L", "XL"],
+        swatch: "orange"
+    },
+    {
+        id: "shirt3",
+        title: "Shirt 3",
+        description: "text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb",
+        price: 25,
+        images: [],
+        sizes: ["S", "M", "L", "XL"],
+        swatch: "black"
+    },
+    {
+        id: "shirt4",
+        title: "Shirt 4",
+        description: "text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb",
+        price: 25,
+        images: [],
+        sizes: ["S", "M", "L", "XL"],
+        swatch: "black"
+    },
+    {
+        id: "shirt5",
+        title: "Shirt 5",
+        description: "text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb",
+        price: 25,
+        images: [],
+        sizes: ["S", "M", "L", "XL"],
+        swatch: "blue"
+    },
+    {
+        id: "shirt6",
+        title: "Shirt 6",
+        description: "text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb",
+        price: 25,
+        images: [],
+        sizes: ["S", "M", "L", "XL"],
+        swatch: "orange"
+    },
+    {
+        id: "shirt7",
+        title: "Shirt 7",
+        description: "text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb text blurb text blurb more text its a blurb",
+        price: 25,
+        images: [],
+        sizes: ["S", "M", "L", "XL"],
+        swatch: "black"
+    }
+];
